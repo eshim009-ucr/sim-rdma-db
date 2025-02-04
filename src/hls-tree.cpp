1a564c704e18fb065338b7346e1765d6e8d9d4f5
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-02-03 17:50:05 -0800

    Updated with latest tree code

--- src/hls-tree.cpp
+++ src/hls-tree.cpp
@@ -1,37 +1,41 @@
+extern "C" {
+#include "b-link-tree/search.h"
+};
 #include "hls-tree.hpp"
 
+
 void sm_search(
-    Tree& tree,
-    hls::stream<bkey_t> &input,
-    hls::stream<bstatusval_t> &output,
-    hls::stream<int> &search2mem,
-    hls::stream<Node> &mem2search
+	Tree& tree,
+	hls::stream<bkey_t> &input,
+	hls::stream<bstatusval_t> &output,
+	hls::stream<int> &search2mem,
+	hls::stream<Node> &mem2search
 ) {
-    static enum {
-        INIT,
-        READ,
-        RESP
-    } state = INIT;
-    static bkey_t key;
-    static bstatusval_t result;
+	static enum {
+		INIT,
+		READ,
+		RESP
+	} state = INIT;
+	static bkey_t key;
+	static bstatusval_t result;
 
 
-    switch(state) {
-        case INIT:
-            if (!input.empty()) {
-                state = READ;
-            }
-            break;
-        case READ:
-            input.read(key);
-            state = RESP;
-            break;
-        case RESP:
-            if (!output.full()) {
-                result = search(&tree, key);
-                output.write(result);
-                state = INIT;
-            }
-            break;
-    }
+	switch(state) {
+		case INIT:
+			if (!input.empty()) {
+				state = READ;
+			}
+			break;
+		case READ:
+			input.read(key);
+			state = RESP;
+			break;
+		case RESP:
+			if (!output.full()) {
+				result = search(&tree, key);
+				output.write(result);
+				state = INIT;
+			}
+			break;
+	}
 }
