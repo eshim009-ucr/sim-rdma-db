daea25c263d894aee162e5b218151b215c201064
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-05-24 21:27:22 -0700

    Removed contents from HLS operations file that were moved to submodule

--- src/sm_operations.cpp
+++ src/sm_operations.cpp
@@ -1,36 +1,55 @@
 #include "operations.hpp"
 
 
-Request encode_search_req(search_in_t in) {
-	Request req = {
-		.opcode = SEARCH,
-		{ .search = in }
-	};
-	return req;
+bool operator==(const Response& lhs, const Response& rhs) {
+	if (lhs.opcode != rhs.opcode) return false;
+	switch (lhs.opcode) {
+		case NOP: return true;
+		case SEARCH: return
+			lhs.search.status == rhs.search.status &&
+			lhs.search.value.data == rhs.search.value.data;
+		case INSERT: return (uint_fast8_t) lhs.insert == (uint_fast8_t) rhs.insert;
+		default: return memcmp(
+				&lhs + sizeof(Opcode),
+				&rhs + sizeof(Opcode),
+				sizeof(Response)-sizeof(Opcode)
+			) == 0;
+	}
 }
 
-Request encode_insert_req(insert_in_t in) {
-	Request req = {
-		.opcode = INSERT,
-		{ .insert = in }
-	};
-	return req;
+bool operator!=(const Response& lhs, const Response& rhs) {
+	return (lhs == rhs) == false;
 }
 
-Response encode_search_resp(search_out_t out) {
-	Response resp = {
-		.opcode = SEARCH,
-		{ .search = out }
-	};
-	return resp;
-}
-
-Response encode_insert_resp(insert_out_t out) {
-	Response resp = {
-		.opcode = INSERT,
-		{ .insert = out }
-	};
-	return resp;
+std::string to_str(const Response& resp) {
+	std::stringstream ss;
+	switch (resp.opcode) {
+		case NOP:
+			ss << "NOP Response";
+			break;
+		case SEARCH:
+			ss << "Search Response ";
+			if (resp.search.status >= 0 && resp.search.status <= 6) {
+				ss << ERROR_CODE_NAMES[resp.search.status];
+			} else {
+				ss << "UNKNOWN";
+			}
+			ss << '(' << (int) resp.search.status << "), " << resp.search.value.data;
+			break;
+		case INSERT:
+			ss << "Insert Response ";
+			if (resp.search.status >= 0 && resp.search.status <= 6) {
+				ss << ERROR_CODE_NAMES[resp.search.status];
+			} else {
+				ss << "UNKNOWN";
+			}
+			ss << '(' << (int) resp.insert << ')';
+			break;
+		default:
+			ss << "Response Opcode " << (int) resp.opcode;
+			break;
+	}
+	return ss.str();
 }
 
 
